f2d1e7d562c88269c219edabfa143db9b54a13e6
Arun Tomar <tomar.arun@gmail.com>
2025-03-09 21:44:11 -0400

    use sonokai color scheme

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -10,8 +10,9 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "catppuccin-frappe",
+    -- colorscheme = "catppuccin-frappe",
     -- colorscheme = "github_dark",
+    colorscheme = "sonokai",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
