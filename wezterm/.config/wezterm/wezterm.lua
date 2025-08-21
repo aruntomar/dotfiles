fd93f0abf50234459f75d9ba5993ec4de5884c9c
Arun Tomar <tomar.arun@gmail.com>
2025-08-21 16:03:47 -0400

    add some more color schemes for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -26,12 +26,20 @@ function scheme_for_appearance(appearance)
 	end
 end
 
-config.color_scheme = scheme_for_appearance(get_appearance())
--- config.color_scheme = "Canonical Solarized Light"
+-- config.color_scheme = scheme_for_appearance(get_appearance())
+-- config.color_scheme = "Classic Light (base16)"
+-- config.color_scheme = "Ayu Light (Gogh)"
+-- config.color_scheme = "Atelier Lakeside Light (base16)"
+-- config.color_scheme = "Horizon Bright (Gogh)"
+-- config.color_scheme = "Summerfruit Light (base16)"
+-- config.color_scheme = "Atelier Lakeside Light (base16)"
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Medium" })
 -- config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 12
+
+-- Add this line to set DPI for 200% scaling on 4K screens
+-- config.dpi = 192.0
 
 -- and finally, return the configuration to wezterm
 return config
